2c219a9eadb56269622d262b067eb727f3a26720
Force Charlie <charlieio@outlook.com>
2020-12-09 21:50:07 +0800

    Update test code

--- test/elfgo/elfgo.go
+++ test/elfgo/elfgo.go
@@ -14,11 +14,19 @@ func main() {
 	}
 	file, err := elf.Open(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "usage: %s elf\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "open file: %s elf error: %v\n", os.Args[0], err)
 		os.Exit(1)
 	}
 	defer file.Close()
 	for _, s := range file.Sections {
 		fmt.Fprintf(os.Stderr, "%s: %08x %d %d %d\n", s.Name, s.Addr, s.Size, s.Entsize, s.Flags)
+	}
+	libs, err := file.ImportedLibraries()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ImportedLibraries: %s elf error: %v\n", os.Args[0], err)
+		os.Exit(1)
+	}
+	for _, l := range libs {
+		fmt.Fprintf(os.Stderr, "need: %s\n", l)
 	}
 }
